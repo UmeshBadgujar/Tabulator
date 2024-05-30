a914f0897efc490ddf989b05379e143fa9aae783
UmeshBadgujar <umesh.b@peerbits.com>
2024-05-30 14:12:53 +0530

    tabulator-changes-add

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index(Request $request)
+    {
+        // Initialize query
+        $query = User::query();
+
+        // Filtering
+        if ($request->has('name')) {
+            $query->where('name', 'like', '%' . $request->input('name') . '%');
+        }
+        if ($request->has('email')) {
+            $query->where('email', 'like', '%' . $request->input('email') . '%');
+        }
+        if ($request->has('phone_number')) {
+            $query->where('phone_number', 'like', '%' . $request->input('phone_number') . '%');
+        }
+        if ($request->has('location')) {
+            $query->where('location', 'like', '%' . $request->input('location') . '%');
+        }
+        if ($request->has('dob')) {
+            $query->where('dob', $request->input('dob'));
+        }
+
+        // Sorting
+        if ($request->has('sort_by')) {
+            $sortBy = $request->input('sort_by');
+            $sortOrder = $request->input('sort_order', 'asc'); // Default to ascending
+            if (in_array($sortBy, ['name', 'email', 'phone_number', 'dob', 'location', 'gender','favourite'])) {
+                $query->orderBy($sortBy, $sortOrder);
+            }
+        }
+
+        $users = $query->paginate(10);
+
+        return response()->json($users);
+    }
+
+    public function update_user(Request $request){
+        try{
+            $input = $request->all();
+            $user = User::find($input['id']);
+
+            if(!empty($user)){
+
+                $user->name = isset($input['name']) ? $input['name']:$user->name;
+                $user->email = isset($input['email']) ? $input['email']:$user->email;
+                $user->phone_number = isset($input['phone_number']) ? $input['phone_number']:$user->phone_number;
+                $user->location = isset($input['location']) ? $input['location']:$user->location;
+                $user->gender = isset($input['gender']) ? $input['gender']:$user->gender;
+                $user->favourite = isset($input['favourite']) ? $input['favourite']:$user->favourite;
+                $user->dob = isset($input['dob']) ? date('y-m-d',strtotime($input['dob'])):$user->dob;
+                $user->save();
+
+                return response()->json(['success' => 1,'msg' => 'User has been updated successfully.'], 200);
+            }else{
+                return response()->json(['error' => "This user doesn't exist."], 201);
+            }
+           
+        }catch(Exception $e){
+            return response()->json(['error' => 'Something went wrong'], 201);
+        }
+    }
+}
---
+++ database/factories/UserFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    protected $model = \App\Models\User::class;
+  
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'phone_number' => $this->faker->unique()->phoneNumber,
+            'location' => $this->faker->randomElement(['United Kingdom', 'Germany','France','Canada','Russia','India','China','South korea']),
+            'gender' => $this->faker->randomElement(['male', 'female']),
+            'favourite' => $this->faker->randomElement(['yellow','blue','orange','red']),
+            'dob' => $this->faker->date
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
